365b2c31d3ee8a5c7fedbf3fc09fcae69329a3c9
henry <henrydbeveridge@gmail.com>
2022-03-29 22:41:25 -0700

    Add very simple login

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-var term = $('#term').terminal({
-    hello: function(name) {
-        this.echo('Hello ' + name + '!');
-    }
-},{
-    prompt: '> ',
-    completion: true,
+$(function() {
+    var term = $('#term').terminal({
+        hello: function(name) {
+            this.echo('Hello ' + name + '!');
+        }
+    },
+    {
+        prompt: '[[;red;]>>> ]',
+        completion: true,
+        greetings: greetings.innerHTML,
+        login: function(user, password) {
+            if (user === 'admin' && password === 'password') {
+                return Promise.resolve('TOKEN');
+            }
+            return Promise.reject();
+        },
+    
+    });
 });
